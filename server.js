a1a2cc6345cd17c0f78e55a816eef606b7a812b5
myself <akwerosophia@gmail.com>
2025-11-06 19:41:20 +0300

    first commit for week01 contacts project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongodb = require("./data/database");
+const route = require("./routes/index");
+
+const port = process.env.PORT || 3000;
+
+app.use("/", route);
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node running on port ${port}`)
+        });
+    }
+});
